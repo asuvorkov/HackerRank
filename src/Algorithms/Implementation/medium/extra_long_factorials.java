aa369f609c5b55c3b63593f3470aea066b595992
asuvorkov <andrey.suvorkov@gmail.com>
2017-06-18 13:54:36 +0200

    Extra long factorials

---
+++ src/Algorithms/Implementation/medium/extra_long_factorials.java
@@ -0,0 +1,37 @@
+package Algorithms.Implementation.medium;
+
+/**
+ * Created by Andrei on 18.06.2017.
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class extra_long_factorials {
+
+    // Returns Factorial of N
+    static BigInteger factorial(int N)
+    {
+        // Initialize result
+        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE
+
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= N; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+
+        return f;
+    }
+
+    // Driver method
+    public static void main(String args[]) throws Exception
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int N = n;
+        System.out.println(factorial(N));
+    }
+}
