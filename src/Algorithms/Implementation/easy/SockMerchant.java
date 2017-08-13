4ddbf9385d8d89a1a514f9e9f32f4c208e702d68
asuvorkov <andrey.suvorkov@gmail.com>
2017-08-13 09:09:49 +0200

    30 days of code - SockMerchant

---
+++ src/Algorithms/Implementation/easy/SockMerchant.java
@@ -0,0 +1,36 @@
+package Algorithms.Implementation.easy;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Created by Andrei on 13.08.2017.
+ */
+public class SockMerchant {
+    static int sockMerchant(int n, int[] ar) {
+        // Complete this function
+        int numberOfPairs = 0;
+        Arrays.sort( ar );
+        for (int i = 1; i < ar.length; i++){
+            if (ar[i] == ar[i - 1]){
+                numberOfPairs++;
+                i++;
+            }
+        }
+        return numberOfPairs;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+    }
+}
