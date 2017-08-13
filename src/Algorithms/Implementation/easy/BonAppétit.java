074b2a2dfca53e7db9ded0cd17753d8c9451e46c
asuvorkov <andrey.suvorkov@gmail.com>
2017-08-13 08:58:12 +0200

    30 days of code - BonAppétit

---
+++ src/Algorithms/Implementation/easy/BonAppétit.java
@@ -0,0 +1,40 @@
+package Algorithms.Implementation.easy;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Created by Andrei on 13.08.2017.
+ */
+public class BonAppétit {
+    static int bonAppetit(int n, int k, int b, int[] ar) {
+        // Complete this function
+        int realBill = 0;
+        for (int i = 0; i < ar.length; i++){
+            if (i != k){
+                realBill += ar[i];
+            }
+        }
+        return realBill / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int b = in.nextInt();
+        int result = bonAppetit(n, k, b, ar);
+        if (b == result){
+            System.out.println("Bon Appetit");
+        }else {
+            System.out.println(b - result);
+        }
+    }
+}
